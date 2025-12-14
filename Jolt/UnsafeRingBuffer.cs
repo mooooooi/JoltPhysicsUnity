3a79048b9d6563b25fb3a7734f7a69c15efb6243
mooooooi <emwings@outlook.com>
2025-12-15 01:37:10 +0800

    容错

--- Jolt/UnsafeRingBuffer.cs
+++ Jolt/UnsafeRingBuffer.cs
@@ -98,6 +98,12 @@ namespace Jolt
 
         public bool TryGetValue(uint sequence, out Span<byte> buffer)
         {
+            if (m_Capacity == 0)
+            {
+                buffer = Span<byte>.Empty;
+                return false;
+            }
+            
             var index = sequence % m_Capacity;
             var entry = m_Entries[index];
             if (entry.SequenceLShift1 == 0 || entry.SequenceLShift1 >> 1 != sequence)
