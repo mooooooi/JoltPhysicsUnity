20d39307add443c57c52c4985539cd3ee0c86aa0
mooooooi <emwings@outlook.com>
2026-03-16 22:41:18 +0800

    feat: add DebuggerDrawSettings C# warpping

--- Jolt/JoltUnityDebugRenderer.cs
+++ Jolt/JoltUnityDebugRenderer.cs
@@ -5,9 +5,60 @@ using UnityEngine;
 
 namespace Jolt
 {
+    [Flags]
+    public enum DebuggerDrawFlags
+    {
+        GetSupportFunction = 1 << 0,
+        SupportDirection = 1 << 1,          
+        GetSupportingFace = 1 << 2,          
+        Shape = 1 << 3,
+        ShapeWireframe = 1 << 4,
+        BoundingBox = 1 << 5,
+        CenterOfMassTransform = 1 << 6,
+        WorldTransform = 1 << 7,
+        Velocity = 1 << 8,
+        MassAndInertia = 1 << 9,
+        SleepStats = 1 << 10,
+        SoftBodyVertices = 1 << 11,
+        SoftBodyVertexVelocities = 1 << 12,
+        SoftBodyEdgeConstraints = 1 << 13,
+        SoftBodyBendConstraints = 1 << 14,
+        SoftBodyVolumeConstraints = 1 << 15,
+        SoftBodySkinConstraints = 1 << 16,
+        SoftBodyLRAConstraints = 1 << 17,
+        SoftBodyPredictedBounds = 1 << 18
+    }
+    
+    public enum ShapeColor : uint
+    {
+        InstanceColor,
+        ShapeTypeColor,
+        MotionTypeColor,
+        SleepColor,
+        IslandColor,
+        MaterialColor,
+    }
+    
+    public enum SoftBodyConstraintColor : uint
+    {
+        ConstraintType,
+        ConstraintGroup,
+        ConstraintOrder,
+    }
+    
+    [Serializable]
+    public struct DebuggerDrawSettings
+    {
+        public DebuggerDrawFlags Flags;
+        public SoftBodyConstraintColor drawSoftBodyConstraintColor;
+    }
+    
     public class JoltUnityDebugRenderer : MonoBehaviour
     {
         public bool Draw;
+        public DebuggerDrawSettings Settings;
+        public ShapeColor ShapeColor;
+        public SoftBodyConstraintColor SoftBodyConstraintColor;
         
         private DebugRenderer m_DebugRenderer;
         private JPH_DrawSettings m_DrawSettings;
@@ -16,8 +67,8 @@ namespace Jolt
         private unsafe void Awake()
         {
             m_DebugRenderer = DebugRenderer.Create(null);
-            fixed(JPH_DrawSettings* settingsPtr = &m_DrawSettings)
-                UnsafeBindings.JPH_DrawSettings_InitDefault(settingsPtr);
+            
+            updateSettings();
             
             m_BodyFilter = BodyDrawFilter.Create(null);
         }
@@ -62,5 +113,43 @@ namespace Jolt
             m_BodyFilter.Destroy();
             m_BodyFilter = default;
         }
+
+        private byte getFlagByte(DebuggerDrawFlags flag)
+        {
+            return (Settings.Flags & flag) > 0 ? (byte)1 : (byte)0;
+        }
+        
+        private void updateSettings()
+        {
+            m_DrawSettings.drawBoundingBox = getFlagByte(DebuggerDrawFlags.BoundingBox);
+            m_DrawSettings.drawCenterOfMassTransform = getFlagByte(DebuggerDrawFlags.CenterOfMassTransform);
+            m_DrawSettings.drawGetSupportFunction = getFlagByte(DebuggerDrawFlags.GetSupportFunction);
+            m_DrawSettings.drawGetSupportingFace = getFlagByte(DebuggerDrawFlags.GetSupportingFace);
+            m_DrawSettings.drawMassAndInertia = getFlagByte(DebuggerDrawFlags.MassAndInertia);
+            m_DrawSettings.drawShape = getFlagByte(DebuggerDrawFlags.Shape);
+            m_DrawSettings.drawShapeWireframe = getFlagByte(DebuggerDrawFlags.ShapeWireframe);
+            m_DrawSettings.drawSleepStats = getFlagByte(DebuggerDrawFlags.SleepStats);
+            m_DrawSettings.drawSoftBodyBendConstraints = getFlagByte(DebuggerDrawFlags.SoftBodyBendConstraints);
+            m_DrawSettings.drawSoftBodyEdgeConstraints = getFlagByte(DebuggerDrawFlags.SoftBodyEdgeConstraints);
+            m_DrawSettings.drawSoftBodyLRAConstraints = getFlagByte(DebuggerDrawFlags.SoftBodyLRAConstraints);
+            m_DrawSettings.drawSoftBodyPredictedBounds = getFlagByte(DebuggerDrawFlags.SoftBodyPredictedBounds);
+            m_DrawSettings.drawSoftBodySkinConstraints = getFlagByte(DebuggerDrawFlags.SoftBodySkinConstraints);
+            m_DrawSettings.drawSoftBodyVertexVelocities = getFlagByte(DebuggerDrawFlags.SoftBodyVertexVelocities);
+            m_DrawSettings.drawSoftBodyVertices = getFlagByte(DebuggerDrawFlags.SoftBodyVertices);
+            m_DrawSettings.drawSoftBodyVolumeConstraints = getFlagByte(DebuggerDrawFlags.SoftBodyVolumeConstraints);
+            m_DrawSettings.drawSupportDirection = getFlagByte(DebuggerDrawFlags.SupportDirection);
+            m_DrawSettings.drawVelocity = getFlagByte(DebuggerDrawFlags.Velocity);
+            m_DrawSettings.drawWorldTransform = getFlagByte(DebuggerDrawFlags.WorldTransform);
+
+            m_DrawSettings.drawShapeColor = (JPH_BodyManager_ShapeColor)ShapeColor;
+            m_DrawSettings.drawSoftBodyConstraintColor = (JPH_SoftBodyConstraintColor)SoftBodyConstraintColor;
+        }
+        
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            updateSettings();
+        }
+#endif
     }
 }
